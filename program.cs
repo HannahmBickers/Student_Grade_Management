101d7a6248ae704d9079425bb478dfa4d6304786
HannahmBickers <Hannahmbickers@gmail.com>
2025-02-11 19:54:14 -0600

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Student Grade Management System!");
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+}
